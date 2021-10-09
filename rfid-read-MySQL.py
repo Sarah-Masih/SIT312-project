2818b19d17a9f4f3f55da28d34f9d5696d792151
Sarah-Masih <70310294+Sarah-Masih@users.noreply.github.com>
2021-10-09 18:27:18 +1100

    Update rfid-read-MySQL.py
    Updated code at 6:27pm

--- rfid-read-MySQL.py
+++ rfid-read-MySQL.py
@@ -1,92 +1,101 @@
+# This code will send data collected by RFID sensor to remote database
+
+
+#including packages
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import requests
 import time
 import os
-#import sqlite3 # Install mysql-connector-python BELOW!!
 import mysql.connector
-# pip install mysql-connector-python
 from datetime import datetime
+
+# setting up raspberry pi pins for leds
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 red = 37
 green = 36
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(red, GPIO.OUT)
-# Changing the connection from local sqlite3 database
+
+# Changing the connection from local sqlite3 database to remote mysql database
 # connection = sqlite3.connect('datalogger.db')
-connection = mysql.connector.connect(host='rfidcheckin.cljhbnnuplh1.ap-southeast-2.rds.amazonaws.com',database='rfidDB',user='admin',password='PASSWORD HERE')
+connection = mysql.connector.connect(host='rfidcheckin.cljhbnnuplh1.ap-southeast-2.rds.amazonaws.com',database='rfidDB',user='admin',password='PASSWORD')
 
 cursor = connection.cursor()
 
-#get data from rfid as well as serial number
-# change "name" variable to "info" and accept relevant info
-#instead of name of tag holder
+# location ID for raspberry Pi module, will differ by location/shop. Unique to each reader.
+location_ID = 12345654
+
 class Logger:
+    
     def __init__(self):
         self.values = []
-        #self.card_ID = card_ID
-        #self.now = now
-        #self.data = data
         self.reader= SimpleMFRC522()
-
+    
+    # read details of tag when tag is presented
     def read_card(self):
         card_id = None
         card_id, data = self.reader.read()
         now = datetime.now()
         latest_entry = (card_id, now, data)
         self.values.append(latest_entry)
-        #return card_id, now, data
-            
+    
+    # This function was created during testing for confirmation on connection. It is not required now.
+    def create_table(self):
+        cursor.execute("CREATE TABLE IF NOT EXISTS sarah_testing (card_id VARCHAR(255), date_time VARCHAR(255), tag_info VARCHAR(255))")
+    
+    # This function will send out data to the database
     def store_data(self):
-        #print(self.data_dict)
-        #for table, data in self.data_dict.items():
-            #print(data)
-        #values = (self.card_ID, self.now, self.data)
-        #print(values)
-        #cnt = len(data)-1
-            #params = '?' + ',?'*cnt
+
         latest_row = self.values[-1]
+        
         card_id = latest_row[0]
         date_time = latest_row[1]
         tag_info = latest_row[2]
         
-        values_to_insert = (card_id, date_time, tag_info)
+        # 12 characters for tag info total. trim excess.
+        required_tag_info = tag_info[0:12]
+         
+        #inserting data in predefined table
+        sql = "INSERT INTO rfidDB.taps (tag_id, time, user_id) VALUES (%s, %s, %s)"  
+        val = (card_id, date_time, required_tag_info)
         
-       # sql = "INSERT INTO Tag (Tag_ID_number, Time_recorded, Information) VALUES (%s, %s, %s)"
-        #sql = "INSERT INTO Tag (Tag_ID_number, Time_recorded, Information) VALUES ('12345678', '20120618 10:34:09 AM', '1234Test_Data')" 
-       # cursor.execute(sql, values_to_insert)
-        cursor.execute("INSERT INTO Tag (Tag_ID_number, Time_recorded, Information) VALUES ('12345678', '20120618 10:34:09 AM', '1234Test_Data')")
+        # uploading recorded data
+        cursor.execute(sql,val)
+        
+        # saving data onto database in respective table
         connection.commit()
-        
-        print(cursor.rowcount, "record inserted")
-        
-            
-          
+        # ideally, location id will also be sent along with this data to the database to identify which reader has read this ID
+    # controlling Green LED and Buzzer through green led code
     def green_led(self):
         GPIO.output(green, GPIO.HIGH)
         time.sleep(1)
         GPIO.output(green, GPIO.LOW)
-        
+    
+    # Controlling red led, buzzer will not ring at unsuccessful reading
     def red_led(self):
         GPIO.output(red, GPIO.HIGH)
         time.sleep(1)
         GPIO.output(red, GPIO.LOW)
-
+    
+   # checking data before adding to database 
     def data_check(self):
         
-       # last_entry = list(self.data_dict.keys())[-1]
+        # reading data from the latest row
         latest_row = self.values[-1]
+        
         card_id = latest_row[0]
         date_time = latest_row[1]
         tag_info = latest_row[2]
-        print(tag_info[0:4])
-        print(latest_row)
         
-            
+        # using first four digits of tag info to authenticate successful read
         if card_id != None and tag_info[0:4] == "1234":
-            print(card_id)
+            #print(len(str(card_id)))
+            #print("type of time", type(date_time))
             return True
+        
+        # unsucessful read
         else:
             print("Data not read properly, entry not stored in file")
                 
@@ -98,16 +107,23 @@ def main():
     try:
         logger = Logger()
         print("running....")
+       # logger.create_table() // this table was created for testing purposes in db to check connection
+       
+       # continously reading data
         while True:
-
+            
+            # read data from card
             logger.read_card()
+            
+            # check data and see if it is a successful read
             if logger.data_check() == True:
                 logger.store_data()
                 logger.green_led()
             else:
                 logger.red_led()
                 continue
-                 
+    
+    # turn off connection
     except KeyboardInterrupt:
         print("Quit")
         cursor.close()
